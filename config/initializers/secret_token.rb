629911378b594db03547fc1b7509ffe068be4abb
Tet Chan <tetchango@gmail.com>
2013-05-24 21:15:06 +0800

    Created a rails application and added statues updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testflight::Application.config.secret_token = '0bd6a440950ce7df6cb37b0a35a5f78c5f6dba60fe236f363c2799aed5b65fde1b299e9ac508895ea3ef17bdb7546121349f3277c0838739c2924cab4a9b9787'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Testflight::Application.config.session_store :cookie_store, key: '_testflight_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Testflight::Application.config.session_store :active_record_store
